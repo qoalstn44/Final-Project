08379b02a60967347c61e7c8ac93c31818b4ec19
qoalstn44 <qoalstn44@naver.com>
2023-02-17 21:30:25 +0900

    ✨:Feat: LogoIn pager UI 추가 LogoIn page UI 추가중 input UI 수정중 passwordValid 코드 추가 수정중 by@ qoalstn44

--- src/components/Login/SignUpModal.tsx
+++ src/components/Login/SignUpModal.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import styled from 'styled-components';
+import { FaEye } from 'react-icons/fa';
 
 // auth를 사용하는 코드
 
@@ -31,6 +33,18 @@ const SignUpModal: React.FC<SignUpModalProps> = ({
 
     try {
       setLoading(true);
+
+      // 이메일 중복 확인
+      const signInMethods = await firebase
+        .auth()
+        .fetchSignInMethodsForEmail(email);
+      if (signInMethods.length > 0) {
+        setError('The email is already in use');
+        setLoading(false);
+        return;
+      }
+
+      // 이메일 등록
       await firebase.auth().createUserWithEmailAndPassword(email, password);
       setLoading(false);
       onRequestClose();
@@ -56,44 +70,131 @@ const SignUpModal: React.FC<SignUpModalProps> = ({
 
   return (
     <>
-      <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-        <h2>Sign Up</h2>
-        {error && <div>{error}</div>}
-        <form onSubmit={handleSignUp}>
-          <div>
-            <label>Email</label>
-            <input
-              type="email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              type="password"
-              value={password}
-              onChange={handlePasswordChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-              required
-            />
-          </div>
-          <button type="submit" disabled={loading}>
-            {loading ? 'Loading...' : 'Sign Up'}
-          </button>
-        </form>
-      </Modal>
+      <StyledBock>
+        <StyledModal
+          isOpen={isOpen}
+          // onRequestClose={onRequestClose}
+          ariaHideApp={false}
+        >
+          <CloseButton onClick={onRequestClose}>x</CloseButton>
+          <h2>회원가입</h2>
+
+          {error && <div>{error}</div>}
+          <form onSubmit={handleSignUp}>
+            <Label>이메일</Label>
+            <div>
+              <Input
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              ></Input>
+            </div>
+            <Label>비밀번호</Label>
+
+            <div>
+              <Input
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              ></Input>
+            </div>
+
+            <Label>비밀번호 확인</Label>
+            <div>
+              <Input
+                type="password"
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                required
+              ></Input>
+            </div>
+          </form>
+          <CompleteButton type="submit" disabled={loading}>
+            {loading ? 'Loading...' : '회원가입'}
+          </CompleteButton>
+        </StyledModal>
+      </StyledBock>
     </>
   );
 };
 
 export default SignUpModal;
+
+const Label = styled.label`
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+const CompleteButton = styled.button`
+  width: 30rem;
+  height: 4rem;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  background: #f39340;
+  color: white;
+  border-radius: 40px;
+  border: none;
+  cursor: pointer;
+  :disabled {
+    background: #c6c6c3;
+  }
+`;
+
+const StyledBock = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgb(0, 0, 0, 75%);
+  z-index: -1;
+`;
+
+const StyledModal = styled(Modal)`
+  position: fixed;
+  top: 45rem;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 39rem;
+  height: 33rem;
+  background-color: white;
+  border-radius: 2rem;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+`;
+
+const Input = styled.input`
+  width: 30rem;
+  height: 3rem;
+  margin-top: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 0 10px;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  margin-bottom: 1rem;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 3.5rem;
+  height: 3.5rem;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #ccc;
+  &:hover {
+    color: #333;
+  }
+`;
