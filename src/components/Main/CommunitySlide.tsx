e2cf8ccadb39a8795ce33e172e97da57e5947d49
joyounghyeun <whdydgus3902@naver.com>
2023-02-23 20:39:53 +0900

    :sparkles:Feat:div로 받아오는것 까지 성공 map함수로 돌려주는것만 구현하면 될 것 같습니다. by @joyounghyeun

--- src/components/Main/CommunitySlide.tsx
+++ src/components/Main/CommunitySlide.tsx
@@ -3,35 +3,33 @@ import { Navigate } from 'react-router';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router';
 import { dbService } from '../../common/firebase';
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  QuerySnapshot,
+} from 'firebase/firestore';
 
 function CommunitySlide() {
   const [popular, setPopular] = useState({ id: '' });
   const navigate = useNavigate();
+  const [userData, setUserData] = useState<any>([]);
 
   useEffect(() => {
     const getData = async () => {
-      //   const posts = { id: 'yfjJR9ITPg5XOC6vVz6f' }; // replace "123" with the actual ID of the post you want to retrieve
-      //   const docRef = doc(dbService, 'posts', posts.id);
-      //   const docSnap = await getDocs(docRef);
-
-      //   const newPopular = {
-      //     id: docSnap.id,
-      //     ...docSnap.data(),
-      //   };
-
-      //   setPopular(newPopular);
-      // };
-      // getData();
       const querySnapshot = await getDocs(collection(dbService, 'posts'));
       console.log(querySnapshot);
-      const dataCall = querySnapshot.forEach((doc) => {
+      let PushData: any = [];
+      querySnapshot.forEach((doc) => {
         console.log(doc.data());
+        PushData.push(doc.data());
       });
+      console.log('확인', PushData);
+      setUserData(PushData);
     };
     getData();
   }, []);
-
   return (
     <div>
       <h1
@@ -42,7 +40,7 @@ function CommunitySlide() {
         뉴스피드
       </h1>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <DDD></DDD>
+        <DDD>{userData[0]?.contents}</DDD>
         <DDD></DDD>
         <DDD></DDD>
       </div>
